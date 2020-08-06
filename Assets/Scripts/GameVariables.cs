5203540e0549afe6d974b1256b195b9729afcfbb
ddduke <kikilefou0001@gmail.com>
2020-08-06 23:47:24 +0200

    Initial commit

---
+++ Assets/Scripts/GameVariables.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameVariables : MonoBehaviour
+{
+    public float Maximum_Distance_Between_Cities = 2f;
+}
